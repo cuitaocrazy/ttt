be486306e7685b38297159118b816aad4cdc85e7
cuitao <ctllfh@gmail.com>
2022-09-20 13:36:49 +0800

    完善测试自动化

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,50 @@
 const exec = require('child_process').exec
+const fs = require('fs')
+const path = require('path')
+
+const testProjectsDir = './tests'
+
+/**
+ * @type {Array<[string, string]>}
+ */
+const projectInfos = []
+
+function getTestProjects() {
+  if (projectInfos.length == 0) {
+    const testProjects = fs.readdirSync(testProjectsDir)
+    return testProjects.reduce((acc, name) => {
+      const testProjectDir = path.join(testProjectsDir, name)
+      const tsConfig = getTsConfig(testProjectDir)
+      if (tsConfig.compilerOptions?.outFile) {
+        acc.push([testProjectDir, path.join(testProjectDir, tsConfig.compilerOptions.outFile)])
+      }
+      return acc
+    }, projectInfos)
+  }
+  return projectInfos
+}
+
+function getTsConfig(testProject) {
+  try {
+    const tsConfigPath = path.join(testProject, 'tsconfig.json')
+    let tsConfig
+    eval(`tsConfig = ${fs.readFileSync(tsConfigPath, 'utf8')}`)
+    return tsConfig
+  } catch (e) {
+    return {}
+  }
+}
 
 /**
  *
  * @param {IGrunt} grunt
  */
 module.exports = function (grunt) {
-  const tests = ['saga-core', 'saga-redis-repo', 'saga-redis-scheduler']
-
   function tscBuild(dir) {
     return new Promise((resolve, reject) => {
       exec(`tsc -b -v ${dir}`, (err, stdout, stderr) => {
         if (err) {
-          reject(err + stderr)
+          reject(stdout + err + stderr)
         } else {
           resolve(stdout)
         }
@@ -23,7 +56,7 @@ module.exports = function (grunt) {
     return new Promise((resolve, reject) => {
       exec(`npx mocha ${jss.join(' ')} --colors`, (err, stdout, stderr) => {
         if (err) {
-          reject(err + stderr)
+          reject(stdout + err + stderr)
         } else {
           resolve(stdout)
         }
@@ -32,10 +65,9 @@ module.exports = function (grunt) {
   }
 
   grunt.registerTask('buildTest', function () {
-    // grunt.log.write('Logging some stuff...').ok()
     const done = this.async()
 
-    Promise.all(tests.map((name) => `./tests/${name}`).map(tscBuild))
+    Promise.all(getTestProjects().map(([dir, _]) => dir).map(tscBuild))
       .then((stdout) => grunt.log.writeln(stdout))
       .then(() => done(true))
       .catch((stderr) => grunt.log.errorlns(stderr))
@@ -45,8 +77,7 @@ module.exports = function (grunt) {
   grunt.registerTask('runTest', function () {
     const done = this.async()
 
-    // Promise.all(tests.map((name) => `./dist/tests/${name}.js`).map(runTest))
-    runTest(tests.map((name) => `./dist/tests/${name}.js`))
+    runTest(getTestProjects().map(([_, js]) => js))
       .then((stdout) => grunt.log.writeln(stdout))
       .then(() => done(true))
       .catch((stderr) => grunt.log.errorlns(stderr))
