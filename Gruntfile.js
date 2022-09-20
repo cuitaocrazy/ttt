806209b75dc04462019e019685eda07e084e64eb
cuitao <ctllfh@gmail.com>
2022-09-20 14:45:26 +0800

    添加build clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,27 +3,46 @@ const fs = require('fs')
 const path = require('path')
 
 const testProjectsDir = './tests'
+const projectsDir = './src'
+
+/**
+ * @type {Array<[string, string]>}
+ */
+const testProjectInfos = []
 
 /**
  * @type {Array<[string, string]>}
  */
 const projectInfos = []
 
-function getTestProjects() {
-  if (projectInfos.length == 0) {
-    const testProjects = fs.readdirSync(testProjectsDir)
-    return testProjects.reduce((acc, name) => {
-      const testProjectDir = path.join(testProjectsDir, name)
-      const tsConfig = getTsConfig(testProjectDir)
+/**
+ *
+ * @param {string} projectsDir
+ * @param {Array<[string, string]>} cache
+ * @returns {void}
+ */
+function fillProjectInfoCache(projectsDir, cache) {
+  if (cache.length == 0) {
+    const projectDirs = fs.readdirSync(projectsDir)
+    return projectDirs.reduce((acc, name) => {
+      const projectDir = path.join(projectsDir, name)
+      const tsConfig = getTsConfig(projectDir)
       if (tsConfig.compilerOptions?.outFile) {
-        acc.push([testProjectDir, path.join(testProjectDir, tsConfig.compilerOptions.outFile)])
+        acc.push([
+          projectDir,
+          path.join(projectDir, tsConfig.compilerOptions.outFile),
+        ])
       }
       return acc
-    }, projectInfos)
+    }, cache)
   }
-  return projectInfos
 }
 
+/**
+ *
+ * @param {string} testProject
+ * @returns {{[key: string]: string}}
+ */
 function getTsConfig(testProject) {
   try {
     const tsConfigPath = path.join(testProject, 'tsconfig.json')
@@ -35,11 +54,26 @@ function getTsConfig(testProject) {
   }
 }
 
+function getTestProjects() {
+  fillProjectInfoCache(testProjectsDir, testProjectInfos)
+  return testProjectInfos
+}
+
+function getProjects() {
+  fillProjectInfoCache(projectsDir, projectInfos)
+  return projectInfos
+}
+
 /**
  *
  * @param {IGrunt} grunt
  */
 module.exports = function (grunt) {
+  /**
+   *
+   * @param {string} dir
+   * @returns {Promise<string>}
+   */
   function tscBuild(dir) {
     return new Promise((resolve, reject) => {
       exec(`tsc -b -v ${dir}`, (err, stdout, stderr) => {
@@ -52,6 +86,11 @@ module.exports = function (grunt) {
     })
   }
 
+  /**
+   *
+   * @param {string[]} jss
+   * @returns {Promise<string>}
+   */
   function runTest(jss) {
     return new Promise((resolve, reject) => {
       exec(`npx mocha ${jss.join(' ')} --colors`, (err, stdout, stderr) => {
@@ -67,7 +106,11 @@ module.exports = function (grunt) {
   grunt.registerTask('buildTest', function () {
     const done = this.async()
 
-    Promise.all(getTestProjects().map(([dir, _]) => dir).map(tscBuild))
+    Promise.all(
+      getTestProjects()
+        .map(([dir, _]) => dir)
+        .map(tscBuild),
+    )
       .then((stdout) => grunt.log.writeln(stdout))
       .then(() => done(true))
       .catch((stderr) => grunt.log.errorlns(stderr))
@@ -85,4 +128,22 @@ module.exports = function (grunt) {
   })
 
   grunt.registerTask('test', ['buildTest', 'runTest'])
+
+  grunt.registerTask('clean', () => {
+    fs.rmdirSync('dist', { recursive: true, force: true })
+  })
+
+  grunt.registerTask('build', function () {
+    const done = this.async()
+
+    Promise.all(
+      getProjects()
+        .map(([dir, _]) => dir)
+        .map(tscBuild),
+    )
+      .then((stdout) => grunt.log.writeln(stdout))
+      .then(() => done(true))
+      .catch((stderr) => grunt.log.errorlns(stderr))
+      .then(() => done(false))
+  })
 }
